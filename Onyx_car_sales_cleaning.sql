268260732752645bc512a95ca966fd354d0d19e5
djibril968 <108284476+djibril968@users.noreply.github.com>
2024-03-05 22:39:09 +0100

    Update Onyx_car_sales_cleaning.sql

--- Onyx_car_sales_cleaning.sql
+++ Onyx_car_sales_cleaning.sql
@@ -35,6 +35,124 @@ EXEC SP_RENAME 'Car_sales.lastSeen', 'Last_seen', 'COLUMN';
 
 
 
+------- cleaning columns
+
+SELECT * 
+FROM Car_Sales
+where  Car_name LIKE '%!!!%'
+
+SELECT Car_name, REPLACE(Car_name, '!', '') AS new_car
+FROM Car_Sales
+where  Car_name LIKE '%!!!%'
+
+SELECT Car_name, REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(Car_name, '"', ''), '#', ''), '$', ''), '%', ''), '*','') AS new_car
+FROM Car_Sales
+---where  Car_name LIKE '%!!!%'
+
+
+UPDATE Car_Sales
+SET Car_name = REPLACE(Car_name, '!', '')
+
+UPDATE Car_Sales
+SET Car_name = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(Car_name, '"', ''), '#', ''), '$', ''), '%', ''), '*','')
+
+SELECT Car_name, REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(Car_name, '"', ''), '#', ''), '$', ''), '%', ''), '*','') AS new_car
+FROM Car_Sales
+WHERE Car_name IS NOT NULL
+ORDER BY 1
+
+SELECT Car_name, REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(Car_name, '&', ''), '.', ''), '/', ''), '?', ''), 
+					'______',''), ':',''), '___>_', ''), '_', ''), '>', '') AS new_car
+FROM Car_Sales
+WHERE Car_name IS NOT NULL
+ORDER BY 1						
+
+UPDATE Car_sales
+SET Car_name = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(Car_name, '&', ''), '.', ''), '/', ''), '?', ''), 
+					'______',''), ':',''), '___>_', ''), '_', ''), '>', '')
+
+SELECT car_name, TRANSLATE(car_name, '/>___>_"#$%.?:!', '')
+FROM car_sales
+WHERE Car_name IS NOT NULL
+ORDER BY 1
+
+
+SELECT Car_name, REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(car_name, '[', ''), '+', ''), ']', ''), '^^', ''), '|', ''), '<', '')
+						,'~','' ), '\', ''), '=', ''), '»', ''), '«', '')
+FROM Car_Sales
+WHERE Car_name IS NOT NULL
+ORDER BY 1
+
+UPDATE Car_sales
+SET Car_name =REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(car_name, '[', ''), '+', ''), ']', ''), '^^', ''), '|', ''), '<', '')
+						,'~','' ), '\', ''), '=', ''), '»', ''), '«', '')
+
+SELECT car_name, REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(car_name, '×', ''), '¥', ''), '°', ''), '•', ''), '€', '')
+FROM car_sales
+WHERE Car_name IS NOT NULL
+ORDER BY 1
+
+UPDATE Car_sales
+SET Car_name = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(car_name, '×', ''), '¥', ''), '°', ''), '•', ''), '€', '')
+
+01788055184
+		
+
+---seller cat column
+SELECT DISTINCT seller_cat, CASE WHEN Seller_cat = 'gewerblich' THEN 'Commercial'
+								WHEN Seller_cat = 'privat' THEN 'Private'
+								END AS seller_catt
+FROM Car_Sales
+order by 1
+
+UPDATE Car_sales
+SET Seller_cat = CASE WHEN Seller_cat = 'gewerblich' THEN 'Commercial'
+					WHEN Seller_cat = 'privat' THEN 'Private'
+					END
+
+---Offer type
+UPDATE Car_Sales
+SET Offer_type = CASE WHEN Offer_type = 'Gesuch' THEN 'request'
+					WHEN Offer_type = 'Angebot' THEN 'offer'
+					END
+
+--- vehicle type
+
+UPDATE Car_Sales
+SET Vehicle_type = CASE 
+					WHEN Vehicle_type = 'kleinwagen' THEN 'Small_car'
+					WHEN Vehicle_type = 'cabrio' THEN 'Convertible'
+					WHEN Vehicle_type = 'andere' THEN 'Other'
+					WHEN Vehicle_type = 'bus' THEN 'Bus'
+					WHEN Vehicle_type = 'coupe' THEN 'Coupe'
+					WHEN Vehicle_type = 'kombi' THEN 'Combi'
+					WHEN Vehicle_type = 'limousine' THEN 'Limousine'
+					WHEN Vehicle_type = 'suv' THEN 'suv'
+					END
+---Gearbox
+UPDATE Car_Sales 
+SET Gearbox =	CASE 
+					WHEN Gearbox = 'automatik' THEN 'Automatic'
+					WHEN Gearbox = 'manuell' THEN 'Manual'
+					END
+
+---- fuel type
+UPDATE Car_Sales 
+SET fuel_type = CASE 
+					WHEN fuel_type = 'benzin' THEN 'Petrol'
+					WHEN Fuel_type = 'hybrid' THEN 'Hybrid'
+					WHEN Fuel_type = 'cng' THEN 'CNG'
+					WHEN Fuel_type = 'andere' THEN 'Other'
+					WHEN Fuel_type = 'diesel' THEN 'Diesel'
+					WHEN Fuel_type = 'elektro' THEN 'Electric'
+					WHEN Fuel_type = 'lpg' THEN 'LPG'
+					END 
+
+SELECT *
+FROM car_sales
+WHERE Car_name IS NOT NULL
+ORDER BY 1
+
 SELECT *
 FROM Car_Sales
 
